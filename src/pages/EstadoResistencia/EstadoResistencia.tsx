e5a1a22a8c155590209b5078db512fd89818a03f
frank159 <frankcf60@gmail.com>
2025-03-11 23:46:44 -0300

    fffff

--- src/pages/EstadoResistencia/EstadoResistencia.tsx
+++ src/pages/EstadoResistencia/EstadoResistencia.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import * as S from './EstadoResistenciaStyled';
-import HardwareIcon from '@mui/icons-material/Hardware';
 
 const EstadoResistencia: React.FC = () => {
   return (
     <S.MainContainer>
       <S.BeatIcon />
-      <S.WorkInProgress>Trabalho em andamento</S.WorkInProgress>
+      <S.WorkInProgress>necessita envio do material</S.WorkInProgress>
     </S.MainContainer>
   );
 };
